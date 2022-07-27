313bf3349b01c8b8134551eee0339192eada27a6
mehdi-mirzaie78 <107181484+mehdi-mirzaie78@users.noreply.github.com>
2022-07-27 16:17:08 +0430

    HW8-4-solution2

---
+++ HW/HW8/HW8-4-solution2.py
@@ -0,0 +1,26 @@
+# 2022-07-27 16:05:31.567031
+class DiscountError(Exception):
+    pass
+
+
+def apply_discount(price: int, discount: float) -> int:
+    """
+         This Function Calculates the Final Price
+    --------------------------------------------------
+    :param price: price of the product
+    :param discount: discount for specific product
+    :return final: final price with applying discount
+    :raises `DiscountError`
+    --------------------------------------------------
+    """
+    final = int(price * (1 - discount))
+    assert 0 <= final <= price, DiscountError
+    return final
+
+
+print(apply_discount(10000, .2))    # 8000
+print(apply_discount(10000, 1))     # 0
+print(apply_discount(10000, -0.2))  # AssertionError DiscountError
+
+# I really don't understand your questions.
+# I hope this is the answer.
