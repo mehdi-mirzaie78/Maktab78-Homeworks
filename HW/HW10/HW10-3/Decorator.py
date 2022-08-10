4126c2f99d9273f52ed67a9e4968d94e8f357127
mehdi-mirzaie78 <107181484+mehdi-mirzaie78@users.noreply.github.com>
2022-08-10 16:49:05 +0430

    Decorator process_timer with logger

---
+++ HW/HW10/HW10-3/Decorator.py
@@ -0,0 +1,27 @@
+from time import time
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+st_handler = logging.StreamHandler()
+file_handler = logging.FileHandler(f"process_timer.log")
+
+st_format = logging.Formatter("%(asctime)s - %(name)8s - %(msg)8s ")
+
+st_handler.setFormatter(st_format)
+file_handler.setFormatter(st_format)
+
+logger.addHandler(st_handler)
+logger.addHandler(file_handler)
+
+
+def process_timer(func):
+    def wrapper(*args, **kwargs):
+        t = time()
+        res = func(*args, **kwargs)
+        dt = time() - t
+        if res > 100000:
+            logger.info(f"<<{func.__name__}>> function - result = {res} - Finished: {dt:.3f} sec")
+        return res
+    return wrapper
