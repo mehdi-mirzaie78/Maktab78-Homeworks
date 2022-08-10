1f167b9587f67a81b869714ad70a03167f109da7
mehdi-mirzaie78 <107181484+mehdi-mirzaie78@users.noreply.github.com>
2022-08-10 14:58:35 +0430

    Solved HW10-1 Explanation is at the end

---
+++ HW/HW10/HW10-1.py
@@ -0,0 +1,42 @@
+# 2022-08-10 14:05:10.695266
+from builtins import print as p
+
+
+class Indenter:
+
+    def __init__(self):
+        self.number = -1
+
+    def print(self, value):
+        p(self.number * '\t' + str(value))
+
+    def __enter__(self):
+        self.number += 1
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.number -= 1
+        return True
+
+
+# Testing
+with Indenter() as indent:
+    indent.print("Hi")
+    with indent:
+        indent.print("Talk is Cheap!")
+        with indent:
+            indent.print("Show me the code...")
+    indent.print("Torvalds")
+
+# output
+# ----------
+# Hi
+# 	Talk is Cheap!
+# 		Show me the code...
+# Torvalds
+# ________________________________
+
+# Explanation:
+# Every time we use with statement __enter__ is called, we need to indent
+# So number attribute will be added to 1. since we got out of the scope
+# __exit__ is called and, we need to undo what we did so we subtract numer from 1.
