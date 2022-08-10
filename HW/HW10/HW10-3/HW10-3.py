58c459bfaf5a48b52a7b052a41dd05436fe5e5fd
mehdi-mirzaie78 <107181484+mehdi-mirzaie78@users.noreply.github.com>
2022-08-10 19:26:59 +0430

    HW10-3 solved. used cache and process_time For fibonacci and factorial. Explanation is at the end of the file.

--- HW/HW10/HW10-3/HW10-3.py
+++ HW/HW10/HW10-3/HW10-3.py
@@ -1,8 +1,10 @@
 from Decorator import process_timer
+from functools import cache
+from sys import setrecursionlimit
 
 
 @process_timer
-def function(num: int):
+def fibo_no_cache(num: int):
     def fibonacci(n=num):
         if n in (0, 1):
             return n
@@ -12,16 +14,52 @@ def function(num: int):
     return fibonacci()
 
 
-def fact(number: int):
-    if number == 1:
-        return 1
-    else:
-        return number * fact(number - 1)
+@process_timer
+def fibo_cache(num: int):
+    @cache
+    def fibonacci(n=num):
+        if n in (0, 1):
+            return n
+        else:
+            return fibonacci(n - 1) + fibonacci(n - 2)
+
+    return fibonacci()
 
 
-print(fact(3))
+@process_timer
+def fact_no_cache(num):
+    def factorial(number=num):
+        if number == 1:
+            return 1
+        else:
+            return number * factorial(number - 1)
 
-# for i in range(30):
-#     print(fibonacci(i), end=' ')
+    return factorial()
 
-print(function(30))
+
+@process_timer
+def fact_cache(num):
+    @cache
+    def factorial(number=num):
+        if number == 1:
+            return 1
+        else:
+            return number * factorial(number - 1)
+
+    return factorial()
+
+
+print(fibo_cache(35))
+print(fibo_no_cache(35))
+
+# Because of RecursionError maximum recursion depth exceeded
+setrecursionlimit(20000)
+
+print(fact_no_cache(1500))
+print(fact_cache(1500))
+
+# As it is obvious, when we use cache decorator.
+# computer doesn't recalculate repetitive values.
+# Once the calculation is done for a part of a program,
+# In the next calculations, program just uses what has calculated before
+# => Using cache makes calculations and process really fast.
