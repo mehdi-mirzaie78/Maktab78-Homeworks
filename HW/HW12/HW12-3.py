d0a4ca0330ef52e0b7c42b4e0f93fbfb111396a1
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-24 14:25:26 +0430

    Updated HW12-3

--- HW/HW12/HW12-3.py
+++ HW/HW12/HW12-3.py
@@ -1,17 +1,24 @@
 import requests
 import datetime
+import pytz
 
 response = requests.get('https://api.sunrise-sunset.org/json?lat=34.3277&lng=47.0778&date=today').json()
 
 # kermanshah: 34.3277° N, 47.0778° E
 
-# ########################## calculating timezone ##################################
+# ############################ calculating timezone ############################
+# Solution 1
 now_utc = datetime.datetime.utcnow()
 now_local = datetime.datetime.now()
 time_zone = now_local - now_utc
 
+# # Solution 2 for calculating timezone
+# tz = pytz.timezone('Asia/Tehran')
+# time_zone = tz.utcoffset(dt=datetime.datetime.utcnow())
+
+# ######################### converting to local time ###########################
 sunrise = datetime.datetime.strptime(response['results']['sunrise'], "%I:%M:%S %p")
-print('sunrise:', (sunrise + time_zone).time())
+print('Sunrise:', (sunrise + time_zone).time())
 
 sunset = datetime.datetime.strptime(response['results']['sunset'], "%I:%M:%S %p")
-print('sunset', (sunset + time_zone).time())
+print('Sunset:', (sunset + time_zone).time())
