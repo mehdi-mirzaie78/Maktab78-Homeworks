1f94a031023459efcd29e60cd8544f09226c7a78
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-24 16:37:46 +0430

    created module for calculating sunrise and sunset

--- HW/HW12/HW12-3.py
+++ HW/HW12/HW12-3.py
@@ -22,3 +22,17 @@ print('Sunrise:', (sunrise + time_zone).time())
 
 sunset = datetime.datetime.strptime(response['results']['sunset'], "%I:%M:%S %p")
 print('Sunset:', (sunset + time_zone).time())
+
+# module
+# ##############################################################################
+def sun_rise_set(timezone_name: str, latitude: str, longitude: str) -> dict[datetime.time]:
+    response = requests.get('https://api.sunrise-sunset.org/json', params={'lat': latitude, 'lng': longitude}).json()
+    tz = pytz.timezone(timezone_name)
+    time_zone = tz.utcoffset(dt=datetime.datetime.utcnow())
+    sunrise = datetime.datetime.strptime(response['results']['sunrise'], "%I:%M:%S %p")
+    sunset = datetime.datetime.strptime(response['results']['sunset'], "%I:%M:%S %p")
+    result = {'sunrise': str((sunrise + time_zone).time()), 'sunset': str((sunset + time_zone).time())}
+    return result
+
+
+print(sun_rise_set('Asia/Tehran', '34.3277', '47.0778'))
