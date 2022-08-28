62284414809643c4e0c72fd2c2763cd3bc6120ad
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-29 01:45:46 +0430

    Deleted user class

--- HW/HW13/core/models.py
+++ HW/HW13/core/models.py
@@ -7,19 +7,3 @@ class DBModel(ABC):  # abstract base Database model
 
     def __str__(self) -> str:
         return f"<{self.__class__.__name__} {vars(self)}>"
-
-
-class User(DBModel):
-    TABLE = 'users'
-    PK = 'id'
-
-    def __init__(self, fullname, username, password, email, id=None):
-        self.fullname = fullname
-        self.username = username
-        self.password = password
-        self.email = email
-        if id:
-            self.id = id
-
-
-user = User('mehdi', 23, '09182251753', 'mehdi@gmail.com')
