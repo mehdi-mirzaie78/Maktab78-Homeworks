f43d50df6f4fe763f4f26bcdac2ebe7e6ae89868
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-30 02:34:55 +0430

    Used Validator class for validating user data

--- HW/HW13/users/models.py
+++ HW/HW13/users/models.py
@@ -1,16 +1,19 @@
 from core.models import DBModel
+from core.utils import Validator
 
 
 class User(DBModel):  # User model
     TABLE = 'users'
     PK = 'id'
 
-    def __init__(self, first_name, last_name, phone, national_id, age, password, id=None) -> None:
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone = phone
-        self.national_id = national_id
-        self.age = age
-        self.password = password
+    def __init__(self, first_name, last_name, national_id, password, phone=None, age=None, id=None) -> None:
+        self.first_name = Validator.name_validator(first_name)
+        self.last_name = Validator.name_validator(last_name)
+        self.national_id = Validator.national_id_validator(national_id)
+        self.password = Validator.pass_validator(password)
+        if phone:
+            self.phone = Validator.phone_validator(phone)
+        if age:
+            self.age = Validator.age_validator(age)
         if id:
             self.id = id
