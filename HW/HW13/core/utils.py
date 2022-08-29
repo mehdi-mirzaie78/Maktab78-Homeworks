c2c190e65a21b4f0a5e5c1ef39bf4b27dae737ff
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-30 03:24:25 +0430

    Added Logger

--- HW/HW13/core/utils.py
+++ HW/HW13/core/utils.py
@@ -1,6 +1,24 @@
 from os import name as os_name, system as terminal
+import logging
 
 
 def clear():
     terminal('cls' if os_name.lower() == 'nt' else 'clear')
 
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('logs.log')
+
+log_format = logging.Formatter("%(asctime)s -\t %(name)s - %(levelname)s\t - %(message)s")
+
+console_handler.setFormatter(log_format)
+file_handler.setFormatter(log_format)
+
+console_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
