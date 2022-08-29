88582d4651071f3fc39c8c8aaedf507eaebe94e6
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-30 03:11:50 +0430

    Added DATABASE parameter to congigs

--- HW/HW13/configs.py
+++ HW/HW13/configs.py
@@ -2,11 +2,12 @@ DB_CONNECTION = {
     "HOST": "localhost",
     "USER": "postgres",
     "PORT": 5432,
-    "PASSWORD": "mehdi377368"
+    "PASSWORD": "mehdi377368",
+    "DATABASE": "filestore"
 }
 
 INFO = {
     "name": "Maktab File Store",
-    "description": "...",
+    "description": "Created By Mehdi Mirzaie on 2022-08-30",
     "version": "1.0.0",
 }
