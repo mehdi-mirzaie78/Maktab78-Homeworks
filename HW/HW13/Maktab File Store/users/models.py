e8fca206b3c47ab7ad47ebf36394cf6a26baa911
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-31 16:30:33 +0430

    Added register method to User class and created Seller class

--- HW/HW13/Maktab File Store/users/models.py
+++ HW/HW13/Maktab File Store/users/models.py
@@ -1,9 +1,9 @@
 from core.models import DBModel
-from core.managers import db
+from core.managers import db, DBManager
 from users.utils import Validator
 
 
-class User(DBModel):  # User model
+class User(DBModel):
     TABLE = 'users'
     PK = 'id'
 
@@ -15,13 +15,56 @@ class User(DBModel):  # User model
         self.balance = Validator.balance_validator(balance)
         if phone:
             self.phone = Validator.phone_validator(phone)
-        if age:
-            self.age = Validator.age_validator(age)
+        if bool(age):
+            self.age = Validator.age_validator(int(age))
         if id:
             self.id = id
 
+    def register_in_database(self, dbman: DBManager = db):
+        print('Your User ID is:', dbman.create(self))
+
+    @staticmethod
+    def register():
+        firstname = input("First Name: ")
+        lastname = input("Last Name: ")
+        national_id = int(input("National ID: "))
+        password = input("Password: ")
+        balance = int(input("Balance: "))
+        phone = input("Phone Number: (Skip: Press Enter) ")
+        age = input("Age: (Skip: Press Enter) ")
+        user = User(firstname, lastname, national_id, password, balance, phone, age)
+        user.register_in_database()
+
+
+class Seller(DBModel):
+    TABLE = 'seller'
+    PK = 'id'
+
+    def __init__(self, first_name, last_name, password, id=None):
+        self.first_name = Validator.name_validator(first_name)
+        self.last_name = Validator.name_validator(last_name)
+        self.password = Validator.pass_validator(password)
+        if id:
+            self.id = id
+
+    def register_in_database(self, dbman: DBManager = db):
+        print('Your Seller Id is:', dbman.create(self))
+
+    @staticmethod
+    def register():
+        firstname = input("First Name: ")
+        lastname = input("Last Name: ")
+        password = input("Password: ")
+        seller = Seller(firstname, lastname, password)
+        seller.register_in_database()
+
+
+
+# seller1 = Seller('mahdi', 'farokhi', 'Mahdi1380')
+# seller1.register()
 
 # user1 = User('mehdi', 'mirzaie', 3242157397, 'Mehdi1378', 1000, id=5)
+# user1.register()
 # user2 = User('reza', 'amin', 3242157137, 'Reza1379', 1000)
 # db.create(user1)
 # db.create(user2)
