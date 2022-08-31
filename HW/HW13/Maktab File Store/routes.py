f3cf4c7554c451c661225a851950ec09479d9cc9
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-31 19:41:35 +0430

    added some routes

--- HW/HW13/Maktab File Store/routes.py
+++ HW/HW13/Maktab File Store/routes.py
@@ -1,10 +1,26 @@
 from core.router import Router, Route, CallBack
 
-router = Router("File Store Router", Route(
-    "Main Menu",
-    "Main menu description ...",
-    children=(
-        Route("About us", callback=CallBack("public.utils", "about_us")),
+register_user = Route("Register Users", "Register Users Description...",
+                      callback=CallBack("public.utils", "register_user"))
 
-    )
-))
+register_seller = Route("Register Sellers", "Register Sellers Description ...",
+                        callback=CallBack("public.utils", "register_seller"))
+
+register_users = Route("Register Sellers and Users", children=(register_user, register_user))
+
+login_user = Route("Login User", callback=CallBack("public.utils", "login_user"))
+
+about_us = Route("About us", callback=CallBack("public.utils", "about_us"))
+
+main_menu = Route("Main Menu", "Main menu description ...", children=(about_us, register_user, login_user))
+
+router = Router("File Store Router", main_menu)
+
+# router = Router("File Store Router", Route(
+#     "Main Menu",
+#     "Main menu description ...",
+#     children=(
+#         Route("About us", callback=CallBack("public.utils", "about_us")),
+#
+#     )
+# ))
