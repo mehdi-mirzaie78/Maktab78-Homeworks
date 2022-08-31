7204cc9cff6562a8b725129a6558ecd0f7c556a2
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-08-31 21:04:36 +0430

    Added functionality of getting back on each item

--- HW/HW13/Maktab File Store/core/router.py
+++ HW/HW13/Maktab File Store/core/router.py
@@ -21,14 +21,26 @@ class Route:
         ...
     """
 
-    def __init__(self, name, description=None, callback: CallBack = None, children=None) -> None:
+    def __init__(self, name, description=None, callback: CallBack = None, children=None, parent=None) -> None:
         self.name = name
         self.description = description
         self.callback = callback
         self.children = children
+        self.parent = parent
 
     def run(self):
         print(self.description) if self.description is not None else print()
+
+        if self.parent:
+            control = input('[C]: Continue\n[B]: Back\n[E]: Exit\n => ').upper()
+            if control == 'C':
+                pass
+            elif control == 'B':
+                self.parent.run()
+            elif control == 'E':
+                exit()
+            else:
+                print('Invalid Input')
 
         if self.callback:
             self.callback: CallBack
@@ -45,6 +57,15 @@ class Route:
             children[index]: Route
             children[index].run()
 
+        if self.parent:
+            control = input('[B]: Back\n[E]: Exit\n => ').upper()
+            if control == 'B':
+                self.parent.run()
+            elif control == 'E':
+                exit()
+            else:
+                print('Invalid Input')
+
 
 class Router:
     """
@@ -54,6 +75,9 @@ class Router:
     def __init__(self, name: str, route: Route) -> None:
         self.name = name
         self.route = route
+        if route.children:
+            for child in route.children:
+                child.parent = self.route
 
     def generate(self) -> None:
         clear()
