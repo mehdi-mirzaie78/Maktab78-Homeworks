0bd254577563f9c593337fe007d5757c1a9fc57d
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-09-01 19:36:23 +0430

    Added New Routes to the main menu

--- HW/HW13/Maktab File Store/routes.py
+++ HW/HW13/Maktab File Store/routes.py
@@ -1,34 +1,37 @@
 from core.router import Router, Route, CallBack
 
-# ################################# Users ####################################
+# ################################# USERS ####################################
 register_user = Route("Register Users", "Register Users Description...",
                       callback=CallBack("public.utils", "register_user"))
 
+buy_files = Route("Buy Files", callback=CallBack("public.utils", "buy_files"))
+
 login_user = Route("Login Users", "Login Users Description...",
-                   callback=CallBack("public.utils", "login_user"))
+                   callback=CallBack("public.utils", "login_user"), children=(buy_files,))
 
 users = Route("Users", children=(register_user, login_user))
 
-# ################################# Sellers ####################################
+# ################################# SELLERS ####################################
 register_seller = Route("Register Sellers", "Register Sellers Description ...",
                         callback=CallBack("public.utils", "register_seller"))
+create_files = Route("Create File", callback=CallBack("public.utils", "create_files"))
 
-login_seller = Route("Login Seller", callback=CallBack("public.utils", "login_seller"))
+show_my_files = Route("Show My Files", callback=CallBack("public.utils", "show_my_files"))
+
+login_seller = Route("Login Seller", callback=CallBack("public.utils", "login_seller"),
+                     children=(create_files, show_my_files))
 
 sellers = Route("Sellers", children=(register_seller, login_seller))
 
 # ################################# ABOUT US ####################################
 about_us = Route("About us", callback=CallBack("public.utils", "about_us"))
 
-main_menu = Route("Main Menu", "Main menu description ...", children=(about_us, users, sellers))
+# #################################### EXIT #####################################
+_exit = Route("Exit", callback=CallBack("public.utils", "_exit"))
 
+# ################################# MAIN MENU ###################################
+main_menu = Route("Main Menu", "< Main menu description >\n",
+                  children=(about_us, users, sellers, _exit))
+
+# Creating router for menu
 router = Router("File Store Router", main_menu)
-
-# router = Router("File Store Router", Route(
-#     "Main Menu",
-#     "Main menu description ...",
-#     children=(
-#         Route("About us", callback=CallBack("public.utils", "about_us")),
-#
-#     )
-# ))
