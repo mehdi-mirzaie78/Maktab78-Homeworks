e46b4083ed6a10ce1e835051b54fde74e06b16f0
mehdi-mirzaie78 <mehdimirzaie1378@gmail.com>
2022-09-07 21:29:08 +0430

    Changed some features

--- HW/HW13/Maktab File Store/core/utils.py
+++ HW/HW13/Maktab File Store/core/utils.py
@@ -12,7 +12,7 @@ logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler('logs.log')
 
-log_format = logging.Formatter("%(asctime)s -\t %(name)s - %(levelname)s\t - %(message)s")
+log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
 console_handler.setFormatter(log_format)
 file_handler.setFormatter(log_format)
@@ -20,5 +20,5 @@ file_handler.setFormatter(log_format)
 console_handler.setLevel(logging.DEBUG)
 file_handler.setLevel(logging.DEBUG)
 
-logger.addHandler(console_handler)
+# logger.addHandler(console_handler)
 logger.addHandler(file_handler)
